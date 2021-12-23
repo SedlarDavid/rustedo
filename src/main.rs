957818a39640e40c57a6abaa963f7c299aefa1f0
SedlarDavid <sedlardavid.cz@gmail.com>
2021-12-23 21:51:46 +0100

    basic file separation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+mod user_info;
+
+// Functions for getting user info
+pub use crate::user_info::user_info_funcs;
+
 fn main() {
-    println!("Rustedo! \n a simple Rust notepad");
+    println!("Rustedo!");
+    let subtitle = "a simple Rust notepad.";
+    println!("{}",subtitle);
+
+    let version : f32 = 1.0; 
+    println!("Version {:.1}",version);
+
+    let user_info = user_info_funcs::get_user_info();
+    println!("User: {}", user_info);
 
 }
---
+++ src/user_info.rs
@@ -0,0 +1,5 @@
+pub mod user_info_funcs {
+    pub fn get_user_info() -> String {
+        return String::from("David S");
+    }
+}
